7e32cf38bc044e8f9c66783a9a56ca32d47cdd4f
Valentino Lugli <valentinolugli@gmail.com>
2021-03-20 13:00:04 +0100

    [P1] Initial push, all important files in order

---
+++ practica1/GVGAI-master/src/TSI/Agent.java
@@ -0,0 +1,29 @@
+/**
+ * TSI: Practica 1
+ * Autor: Valentino Lugli; Marzo - 2021
+ **/
+
+// Package Info
+package TSI;
+
+// LIBRARIES
+//	StateObservation
+import core.game.StateObservation;
+//	ACTIONS
+import ontology.Types;
+//	ElapsedCpuTimer
+import tools.ElapsedCpuTimer;
+
+public class Agent
+{
+	public Agent(StateObservation stateObs, ElapsedCpuTimer elapsedTimer)
+	{
+		
+	}
+	
+	public Types.ACTIONS act(StateObservation stateObs, ElapsedCpuTimer elapsedTimer)
+	{
+		
+		return null;	
+	}
+}
